0eb06016974a324d16caff5e705c949bd2d0f37e
Amakaphobia <nospam@gmx.de>
2018-04-15 19:56:19 +0200

    events

---
+++ Rpg/Model/entity/I_EventTarget.java
@@ -0,0 +1,20 @@
+package entity;
+
+import event.I_Event;
+
+/**
+ * This interface will be implemented by every class that can be a target of a {@link I_Event}.
+ * It is functional and its only method is a consumer that is used accept the event. In the implementation it is used
+ * to access the events {@link I_Event#execute(Object)} Method and insert <code>this</code> as its parameters.
+ * @author Dave
+ *
+ */
+@FunctionalInterface
+public interface I_EventTarget
+{
+	/**
+	 * This Method is used to handle {@link I_Event} that target <code>this</code>.
+	 * @param event the event that should be executed.
+	 */
+	public abstract void handleEvent(I_Event<?> event);
+}
---
+++ Rpg/Model/event/I_Event.java
@@ -0,0 +1,22 @@
+package event;
+
+import entity.I_EventTarget;
+
+/**
+ * This Interface is implemented by events (attacks, spells, natural catastrophes,...).
+ * It is a functional Interface, and its only Consumer is the execute method. The Execute Method holds
+ * the code that describes the Event and gets the Target as a Parameter.
+ * @author Dave
+ *
+ * @param <T> The type of the Target
+ */
+@FunctionalInterface
+public interface I_Event<T extends I_EventTarget>
+{
+	/**
+	 * This Method holds the Content of the event. Every action that is a result of a Event and applies to the target
+	 * is taken here.
+	 * @param target the Target of the Event.
+	 */
+	public abstract void execute(T target);
+}
