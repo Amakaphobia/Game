452922983e538ecb03aa5449671a034867f48387
Amakaphobia <nospam@gmx.de>
2018-04-16 21:15:10 +0200

    .

--- Rpg/Model/entity/basic/entity/EntityBase.java
+++ Rpg/Model/entity/basic/entity/EntityBase.java
@@ -2,11 +2,12 @@ package entity.basic.entity;
 
 import java.io.Serializable;
 
-import common.render.I_InfoAble;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
+
+import common.render.I_InfoAble;
 
 /**
  * This class is used as a abstract Base Class for all Person-Classes.
@@ -15,13 +16,13 @@ import javafx.scene.image.Image;
  *
  */
 public abstract class EntityBase implements Serializable, I_InfoAble{
-	
+
 	// TODO ticking?
-	
+
 	/**serial*/
 	protected static final long serialVersionUID = -8965100601145336638L;
-	
-	/**set this to true if the object ticks*/ 
+
+	/**set this to true if the object ticks*/
 	protected boolean living = false;
 	/**
 	 * used to access {@link EntityBase#living}
@@ -33,7 +34,7 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 	 * @param value true if alive
 	 */
 	public void setLiving(boolean value) { this.living = value; }
-	
+
 	/**this StringProperty holds the unique ID of a entity*/
 	protected final StringProperty id;
 	/**
@@ -46,7 +47,7 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 	 * @return this objects id as a String
 	 */
 	public String getId() { return this.id.get(); }
-	
+
 	/**this StringProperty holds the name of the entity*/
 	protected final StringProperty name;
 	/**
@@ -59,7 +60,7 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 	 * @return the name as a String.
 	 */
 	public String getName() { return this.name.get(); }
-	
+
 	/**this StringProperty contains the description*/
 	protected final StringProperty description;
 	/**
@@ -72,14 +73,14 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 	 * @return a string containing the description.
 	 */
 	public String getDescription() { return this.description.get(); }
-	
+
 	/**holds the path to the image*/
 	protected String bildPath;
 	/**the Image used for information*/
 	protected Image bild = null;
 	/**the Parent that is used to render stuff on the map*/
 	protected Parent render = null;
-	
+
 	/**
 	 * Constructor
 	 * @param name the name you want the Entity to have
@@ -92,19 +93,19 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 		this.description = new SimpleStringProperty("");
 		this.bildPath = bildPath;
 	}
-	
+
 	// TODO mach besser
 	/**holds the next ID to be used*/
 	private static int nextID = 0;
 	/**
-	 * this method is used internally by this class to generate the next unique ID to be asigned to the
+	 * this method is used internally by this class to generate the next unique ID to be assigned to the
 	 * {@link EntityBase#id}.
 	 * @return a string containing the next id.
 	 */
 	private final String generateId() {
 		return String.valueOf(nextID++);
-	}	
-	
+	}
+
 	/**
 	 * this method is used to access the RenderAble object of this object. On first use it will use
 	 * {@link EntityBase#buildRender()} to generate the Rendering
@@ -115,43 +116,42 @@ public abstract class EntityBase implements Serializable, I_InfoAble{
 			this.render = this.buildRender();
 		return this.render;
 	}
-	
+
 	/**
 	 * this method is used to access the ImageObject of this object. On first use it will use
-	 * {@link EntityBase#bildPath} to access the image. 
+	 * {@link EntityBase#bildPath} to access the image.
 	 * @return a FX Image for the info panel or dialog windows
 	 */
 	public Image getBild() {
-		if(this.bild == null) {
+		if(this.bild == null)
 			this.bild = new Image(this.bildPath);
-		}
-		return this.bild; 
+		return this.bild;
 	}
-	
+
 	/**
 	 * this method is used to internally to generate the Renderable object for placement on the map.
 	 * @return a Parent for this object
 	 */
 	protected abstract Parent buildRender();
-	
+
 	@Override
 	public abstract Parent getInfoView();
-	
+
 	// Object Methods:
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if(obj == this) return true;
 		if(obj == null) return false;
 		if(!(obj instanceof EntityBase)) return false;
-		
+
 		EntityBase other = (EntityBase) obj;
 		return this.getId().equals(other.getId());
-	}	
+	}
 	@Override
 	public int hashCode() {
 		return this.getId().hashCode();
-	}	
+	}
 	@Override
 	public String toString() {
 		return String.format("EntityBase: %s %s", this.getId(), this.getName());
