00db837eec3db5e446ba982e6eb31fbaeb30a77a
Amakaphobia <nospam@gmx.de>
2018-04-17 16:09:23 +0200

    docu

--- Rpg/Model/common/map/I_GameMap.java
+++ Rpg/Model/common/map/I_GameMap.java
@@ -1,25 +1,26 @@
 package common.map;
 
-import boxes.Pair;
-import entity.basic.entityBase.MoveableEntityBase;
 import javafx.beans.property.IntegerProperty;
 
+import boxes.Pair;
+import entity.basic.entityBase.MoveableEntityBase;
+
 /**
- * This interface is implemented by all gamemap classes. it handles positioning of entities and moveing them.
+ * This interface is implemented by all gamemap classes. it handles positioning of entities and moving them.
  * @author hdaiv_000
  *
  */
 public interface I_GameMap {
-	
+
 	/**
 	 * this method is used to test if a given {@link Pair} of {@link IntegerProperty}s is inside the boundary
-	 * @param Location
-	 * @return
+	 * @param Location the {@link Pair} of {@link IntegerProperty}s you want to test
+	 * @return true if tis inside false if not
 	 */
 	public default boolean testBoundary(Pair<IntegerProperty, IntegerProperty> Location) {
 		return this.testBoundaryX(Location.getKey().get()) && this.testBoundaryY(Location.getValue().get());
 	}
-	
+
 	/**
 	 * tests if a {@link MoveableEntityBase} is inside the maps boundaries
 	 * @param Entity the Entity you want to test
@@ -28,7 +29,7 @@ public interface I_GameMap {
 	public default boolean testBoundary(MoveableEntityBase Entity) {
 		return this.testBoundary(Entity.getLocation());
 	}
-	
+
 	/**
 	 * this method is used to check if a given x-coordinate is inside the map.
 	 * @param coordX the x-position you want to check
@@ -37,7 +38,7 @@ public interface I_GameMap {
 	public abstract boolean testBoundaryX(int coordX);
 	/**
 	 * this method is used to check if a given y-coordinate is inside the map.
-	 * @param coordX the y-position you want to check
+	 * @param coordY the y-position you want to check
 	 * @return true if its inside false if not
 	 */
 	public abstract boolean testBoundaryY(int coordY);
