3777eb877c4672daef5d9e55624aa3c9be0a6121
Amakaphobia <nospam@gmx.de>
2018-04-21 16:47:22 +0200

    docu

--- Rpg/Model/entity/clazz/Clazzs.java
+++ Rpg/Model/entity/clazz/Clazzs.java
@@ -5,22 +5,54 @@ import java.util.List;
 
 import entity.basic.skillSet.Skills;
 
+/**
+ * Enum identifying available classes.
+ * @author Dave
+ *
+ */
 public enum Clazzs implements Serializable
 {
+	/**
+	 * Mage Class
+	 */
 	MAGE("Mage", "1d6"),
+
+	/**
+	 * Warrior Class
+	 */
 	WARRIOR("Warrior", "1d10");
 
+	/**the Class Name*/
 	private String id;
+	/**the hitdie of the class*/
 	private String hitDieCode;
 
+	/**
+	 * Constructor
+	 * @param id the class name
+	 * @param hitDieCode the class' hitdie code
+	 */
 	private Clazzs(String id, String hitDieCode) {
 		this.id = id;
 		this.hitDieCode = hitDieCode;
 	}
 
+	/**
+	 * Access the ClassName
+	 * @return the Class Name
+	 */
 	public String getId() { return this.id; }
+
+	/**
+	 * Access the hitdie code
+	 * @return the hitdie code
+	 */
 	public String getHitDieCode() { return this.hitDieCode; }
 
+	/**
+	 * This Method is used to get a List of all {@link Skills}, which are class skills of this class
+	 * @return List containing all ClassSkills
+	 */
 	public List<Skills> getClassSkills(){
 		return ClazzSkillListGenerator.getClassSkillsFor(this);
 	}
