b4bac192d61f05b6284ca0d780706787c76717b4
Amakaphobia <nospam@gmx.de>
2018-04-21 19:21:16 +0200

    hashcode

--- Rpg/Model/entity/basic/attributeSet/Attribute.java
+++ Rpg/Model/entity/basic/attributeSet/Attribute.java
@@ -92,7 +92,7 @@ public class Attribute implements Serializable
 	}
 	@Override
 	public int hashCode() {
-		int value = this.value.hashCode();
+		int value = Integer.hashCode(this.value.get());
 		int name = this.name.hashCode();
 		return value + name*name;
 	}
