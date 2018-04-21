65d90d8007295914bbf1ef802521a3cf9fc66d37
Amakaphobia <nospam@gmx.de>
2018-04-21 17:52:08 +0200

    test

---
+++ Rpg/Test/entity/SkillTest.java
@@ -0,0 +1,65 @@
+package entity;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("javadoc")
+class SkillTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsUntrained() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNameId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMainAttribute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
