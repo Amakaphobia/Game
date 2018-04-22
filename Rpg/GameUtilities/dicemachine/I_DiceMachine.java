c3a4118a16a15e7c66a96a089c3bc4cdfa5f70b9
Amakaphobia <nospam@gmx.de>
2018-04-22 11:10:55 +0200

    getRollMax()

--- Rpg/GameUtilities/dicemachine/I_DiceMachine.java
+++ Rpg/GameUtilities/dicemachine/I_DiceMachine.java
@@ -26,6 +26,25 @@ public interface I_DiceMachine extends Serializable{
 	public abstract int getRoll(String diceCode);
 
 	/**
+	 * this helper method is used to compute to maximum possible throw with a given diceCode
+	 * @param diceCode the dicecode for your throw
+	 * @return the integer that is the biggest possible result of the given dicecode
+	 */
+	public default int getRollMax(String diceCode) {
+		StringTokenizer strk = new StringTokenizer(diceCode, "d");
+
+		int multiplikator = 0;
+		if(strk.hasMoreTokens())
+			multiplikator = this.getRoll(strk.nextToken());
+
+		int singleErg = 0;
+		if(strk.hasMoreTokens())
+			singleErg = this.getRoll(strk.nextToken());
+
+		return multiplikator * singleErg;
+	}
+
+	/**
 	 * Utility Method used by the {@link I_DiceMachine} implementation to split the dicecode into its component parts
 	 * @param diceCode the code used to describe the dicethrow
 	 * @param delim the delimiter used by the {@link StringTokenizer}.
