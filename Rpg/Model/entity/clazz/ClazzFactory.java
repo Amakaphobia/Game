c14e26a0f7296ea7ffe2e68aa0dc1c4f5b06252e
Amakaphobia <nospam@gmx.de>
2018-04-29 15:21:24 +0200

    mock

---
+++ Rpg/Model/entity/clazz/ClazzFactory.java
@@ -0,0 +1,46 @@
+package entity.clazz;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import entity.actorBase.ClassedActorBase;
+import entity.basic.common.enums.clazz.Clazzs;
+import entity.spellbook.I_SpellStrategy;
+import entity.spellbook.strategies.NonMagical;
+
+public class ClazzFactory {
+
+	public ClazzFactory() {
+		super();
+	}
+
+	public static final ClazzBase warriorMock(ClassedActorBase Parent) {
+		return new WarriorMock(Parent);
+	}
+
+	//TODO remove mock
+	@SuppressWarnings("javadoc")
+	private static class WarriorMock extends ClazzBase{
+
+		public WarriorMock(ClassedActorBase Parent) {
+			super(Clazzs.WARRIOR, Parent);
+		}
+
+		@Override
+		public javafx.scene.Parent getInfoView() {
+			return null;
+		}
+
+		@Override
+		protected Map<Integer, Consumer<? super ClazzBase>> buildLevelStrategies() {
+			return new HashMap<>();
+		}
+
+		@Override
+		protected I_SpellStrategy injectSpellStrategy() {
+			return new NonMagical();
+		}
+
+	}
+}
