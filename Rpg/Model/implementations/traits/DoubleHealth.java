1c292fa718d730657dca5ad2315b902506d9a8e1
Amakaphobia <nospam@gmx.de>
2018-05-05 17:38:18 +0200

    testclass

---
+++ Rpg/Model/implementations/traits/DoubleHealth.java
@@ -0,0 +1,71 @@
+package implementations.traits;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Parent;
+
+import entity.actorBase.ClassedActorBase;
+import entity.basic.attributeSet.AttributeSet;
+import entity.basic.attributeSet.I_AttributeSet;
+import entity.basic.common.enums.size.Sizes;
+import entity.basic.race.RaceBase;
+import entity.basic.traits.TraitBase;
+
+@SuppressWarnings("javadoc") //TODO remove me
+public class DoubleHealth extends TraitBase<ClassedActorBase> {
+
+	public DoubleHealth() {
+		super();
+	}
+
+	@Override
+	public void applyTo(ClassedActorBase traitTarget) {
+		traitTarget.getHp().maxHealthProperty().set(traitTarget.getHp().getMaxHealth() * 2);
+	}
+
+	@Override
+	public void removeFrom(ClassedActorBase traitTarget) {
+		traitTarget.getHp().maxHealthProperty().set(traitTarget.getHp().getMaxHealth() / 2);
+	}
+
+
+
+	public static void main(String[] args) {
+		RaceBase rb = new RaceBase("", Sizes.COLOSSAL) {
+
+			@Override
+			protected I_AttributeSet buildRacialAttributes() {
+				return new AttributeSet(0);
+			}
+
+			@Override
+			protected List<TraitBase<?>> buildRacialTraits() {
+				return new ArrayList<>();
+			}
+
+		};
+		ClassedActorBase cab = new ClassedActorBase("","",null,rb,null,new AttributeSet(5),null,null) {
+
+			@Override
+			public Parent getInfoView() {
+				return null;
+			}
+
+			@Override
+			public Parent buildRender() {
+				return null;
+			}
+		};
+
+		cab.getHp().addHitDie("2");
+		TraitBase<ClassedActorBase> t = new DoubleHealth();
+		cab.addTrait(t);
+
+		System.out.println(cab.getHp().getMaxHealth());
+
+		cab.removeTrait(t);
+
+		System.out.println(cab.getHp().getMaxHealth());
+	}
+}
