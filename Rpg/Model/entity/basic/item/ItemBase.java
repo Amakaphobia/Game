56e213cab2a0fdd1626f634c27a35db499c52cf0
Amakaphobia <nospam@gmx.de>
2018-05-05 19:04:53 +0200

    ItemBase buildup

--- Rpg/Model/entity/basic/item/ItemBase.java
+++ Rpg/Model/entity/basic/item/ItemBase.java
@@ -2,54 +2,98 @@ package entity.basic.item;
 
 import entity.basic.EntityBase;
 import entity.basic.common.enums.item.ItemSlot;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 
+/**
+ * This is the base Implementation Class for all items.
+ *
+ * @see EntityBase
+ *
+ * @author Dave
+ *
+ */
 public abstract class ItemBase extends EntityBase {
 
 	//TODO war's das scho?
-	
+
 	// Constructor
-	
+
+	/**
+	 * Constructor
+	 *
+	 * @see EntityBase#EntityBase(String, String, String)
+	 *
+	 * @param name the name
+	 * @param bildPath the bildPath
+	 * @param description the description
+	 * @param weight the weight
+	 * @param slot the slot
+	 * @param wearable true if it can be worn
+	 * @param itemGroupId the items group id
+	 */
 	public ItemBase(
-			String name, String bildPath, String description, 
+			String name, String bildPath, String description,
 			int weight, ItemSlot slot, boolean wearable, int itemGroupId) {
-		
+
 		super(name, bildPath, description);
-		this.weight = new SimpleIntegerProperty(weight);
+		this.weight = weight;
 		this.slot = slot;
 		this.wearable = wearable;
 		this.itemGroupId = itemGroupId;
 	}
-	
+
+	/**
+	 * Constructor for items that cannot be worn.
+	 *
+	 * @see EntityBase#EntityBase(String, String, String)
+	 *
+	 * @param name the name
+	 * @param bildPath the bildPath
+	 * @param description the description
+	 * @param weight the weight
+	 * @param itemGroupId the items group id
+	 */
+	public ItemBase(
+			String name, String bildPath, String description,
+			int weight, int itemGroupId) {
+		this(name, bildPath, description, weight, ItemSlot.SLOTLESS, false, itemGroupId);
+	}
+
 	// Properties
-	
-	private final IntegerProperty weight;
-	public final int getWeight() { return this.weight.get(); }
-	public final IntegerProperty weightProperty() { return this.weight; }
-	
+
+	/**this items weight*/
+	private final int weight;
+	/**@return the {@link #weight}*/
+	public final int getWeight() { return this.weight; }
+
+	/**this items slot*/
 	private final ItemSlot slot;
-	public ItemSlot getSlot() { return this.slot; }
-	
+	/**@return {@link #slot}*/
+	public final ItemSlot getSlot() { return this.slot; }
+
+	/**true if this item can be worn*/
 	private final boolean wearable;
-	public boolean isWearable() { return this.wearable; }
-	
+	/**@return {@link #wearable}*/
+	public final boolean isWearable() { return this.wearable; }
+
+	/**this items group identifier*/
 	private final int itemGroupId;
+	/**@return {@link #itemGroupId}*/
 	public final int getItemGroupId() { return this.itemGroupId; }
-	
+
 	// Object
-	
+
+	/**<b>does not check entity id</b><br>Items that have the same values are equal.*/
 	@Override
 	public boolean equals(Object obj) {
 		if(obj == this) return true;
 		if(obj == null) return false;
 		if(!(obj instanceof ItemBase)) return false;
-		
+
 		ItemBase other = (ItemBase) obj;
-		
-		return this.wearable == other.wearable
+
+		return this.getName().equals(other.getName())
+			&& this.wearable == other.wearable
 			&& this.weight == other.weight
-			&& this.slot.equals(other.slot)
-			&& super.equals(other);
+			&& this.slot.equals(other.slot);
 	}
 }
